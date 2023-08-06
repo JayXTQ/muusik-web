f138264ab1401208bc868dcb929a456a58d53894
Jay <54886938+JayXTQ@users.noreply.github.com>
2023-08-06 13:15:35 +0100

    Update +page.svelte

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -7,20 +7,30 @@
 	import { goto } from '$app/navigation';
     let fragment = new URLSearchParams($page.url.href.split("#")[1]);
 	const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+    let user: any;
     if(accessToken && tokenType){
-        fetch('https://discord.com/api/users/@me', {
+        user = fetch('https://discord.com/api/users/@me', {
                 headers: {
                     authorization: `${tokenType} ${accessToken}`,
                 },
         }).then(res => res.json()).then(res => {
             if(browser){
-                window.localStorage.setItem('user', JSON.stringify(res.id));
+                window.localStorage.setItem('accessToken', accessToken);
+                window.localStorage.setItem('tokenType', tokenType);
             }
         })
     } 
-    if (browser && window.localStorage.getItem('user')){
-        goto('/dashboard');
+    if (browser && window.localStorage.getItem('accessToken') && window.localStorage.getItem('tokenType')){
+        user = fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${tokenType} ${accessToken}`,
+            },
+        }).then(res => res.json())
     } else if(browser) {
         goto('//api.muusik.app/login');
     }
 </script>
+
+{#await user then u}
+    <pre>{JSON.stringify(user, null, 4)}</pre>
+{/await}
