7edba820f4f52823c73e233c5edcdc85c6658c7c
Jay <me@jayxtq.xyz>
2023-11-15 14:51:19 +0000

    feat: supabase login

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,18 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
-}
+// src/app.d.ts
 
-export {};
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+import { Database } from './DatabaseDefinitions'
+
+declare global {
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient<Database>
+      getSession(): Promise<Session | null>
+    }
+    interface PageData {
+      session: Session | null
+    }
+    // interface Error {}
+    // interface Platform {}
+  }
+}
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	export let data
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
 
-	// export let data: PageData;
-	import { page } from '$app/stores';
-	import { browser } from '$app/environment';
-	import { goto } from '$app/navigation';
+	if(!session) {
+		supabase.auth.signInWithOAuth({ provider: 'discord' })
+	}
+
 	import {
 		P,
 		Heading,
@@ -20,38 +22,6 @@
 		Avatar
 	} from 'flowbite-svelte';
 	import { Icon, Cog6Tooth, Pause, Forward } from 'svelte-hero-icons';
-	let fragment = new URLSearchParams($page.url.href.split('#')[1]);
-	const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
-	let user: any;
-	if (accessToken && tokenType) {
-		user = fetch('https://discord.com/api/users/@me', {
-			headers: {
-				Authorization: `${tokenType} ${accessToken}`
-			}
-		}).then((res) => res.json());
-		if (browser) {
-			window.localStorage.setItem('accessToken', accessToken);
-			window.localStorage.setItem('tokenType', tokenType);
-		}
-	}
-	if (browser) {
-		if (
-			window.localStorage.getItem('accessToken') &&
-			window.localStorage.getItem('tokenType') &&
-			!user
-		)
-			user = fetch('https://discord.com/api/users/@me', {
-				headers: {
-					Authorization: `${tokenType} ${accessToken}`
-				}
-			}).then((res) => res.json());
-		else if (
-			!window.localStorage.getItem('accessToken') &&
-			!window.localStorage.getItem('tokenType') &&
-			!user
-		)
-			goto('//api.muusik.app/login');
-	}
 	let songs: any = [];
 	let timer: any;
 	async function searchSong(e: any) {
@@ -59,7 +29,7 @@
 		clearTimeout(timer);
 		timer = setTimeout(async () => {
 			const query = e.target.value;
-			const res = await fetch(`//localhost:5714/find-song?query=${query}`);
+			const res = await fetch(`//localhost:8000/find-song?query=${query}`);
 			const data = await res.json();
 			if (data.success) {
 				songs = data.song.slice(0, 5);
@@ -95,64 +65,60 @@
 
 <pre class="text-white">{JSON.stringify(songs, null, 4)}</pre>
 
-{#await user then u}
-	<!-- <pre>{JSON.stringify(u, null, 4)}</pre> -->
-
-	<div class="flex flex-col justify-center h-screen mr-56 w-full -mt-6">
-		<div class="bg-primary-100 rounded-xl mx-[5.63rem] grow max-h-96 flex-col flex">
-			<div class="flex m-4">
-				<Heading tag="h2" class="text-white font-inter grow">The Queue</Heading>
-				<Heading tag="h2" class="text-white font-inter w-fit">{u.username}</Heading>
-			</div>
-			<div class="grow">
-				<Table color="custom" class="text-white font-inter">
-					<TableHead>
-						<TableHeadCell>Track</TableHeadCell>
-						<TableHeadCell>Artist</TableHeadCell>
-						<TableHeadCell>Album</TableHeadCell>
-						<TableHeadCell>Duration</TableHeadCell>
-						<TableHeadCell>Added By</TableHeadCell>
-					</TableHead>
-					<TableBody>
-						<!-- {#each data.queue as track}
-                            <TableBodyRow>
-                                <TableBodyCell>{track.name}</TableBodyCell>
-                                <TableBodyCell>{track.artist}</TableBodyCell>
-                                <TableBodyCell>{track.album}</TableBodyCell>
-                                <TableBodyCell>{track.duration}</TableBodyCell>
-                                <TableBodyCell>{track.addedBy}</TableBodyCell>
-                            </TableBodyRow>
-                        {/each} -->
-					</TableBody>
-				</Table>
-			</div>
-			<Heading tag="h2" class="text-white font-inter h-fit m-4 w-fit">x songs left out of x</Heading
-			>
+<div class="flex flex-col justify-center h-screen mr-56 w-full -mt-6">
+	<div class="bg-primary-100 rounded-xl mx-[5.63rem] grow max-h-96 flex-col flex">
+		<div class="flex m-4">
+			<Heading tag="h2" class="text-white font-inter grow">The Queue</Heading>
+			<Heading tag="h2" class="text-white font-inter w-fit">{session?.user.user_metadata.name}</Heading>
 		</div>
+		<div class="grow">
+			<Table color="custom" class="text-white font-inter">
+				<TableHead>
+					<TableHeadCell>Track</TableHeadCell>
+					<TableHeadCell>Artist</TableHeadCell>
+					<TableHeadCell>Album</TableHeadCell>
+					<TableHeadCell>Duration</TableHeadCell>
+					<TableHeadCell>Added By</TableHeadCell>
+				</TableHead>
+				<TableBody>
+					<!-- {#each data.queue as track}
+						<TableBodyRow>
+							<TableBodyCell>{track.name}</TableBodyCell>
+							<TableBodyCell>{track.artist}</TableBodyCell>
+							<TableBodyCell>{track.album}</TableBodyCell>
+							<TableBodyCell>{track.duration}</TableBodyCell>
+							<TableBodyCell>{track.addedBy}</TableBodyCell>
+						</TableBodyRow>
+					{/each} -->
+				</TableBody>
+			</Table>
+		</div>
+		<Heading tag="h2" class="text-white font-inter h-fit m-4 w-fit">x songs left out of x</Heading
+		>
+	</div>
 
-		<Input
-			size="lg"
-			type="text"
-			name="query"
-			placeholder="Search"
-			class="bg-primary-100 text-white mx-auto rounded-xl mt-8 font-inter border-primary-200 border-4 w-full xl:max-w-[48.9rem]"
-			on:keyup={searchSong}
-		/>
+	<Input
+		size="lg"
+		type="text"
+		name="query"
+		placeholder="Search"
+		class="bg-primary-100 text-white mx-auto rounded-xl mt-8 font-inter border-primary-200 border-4 w-full xl:max-w-[48.9rem]"
+		on:keyup={searchSong}
+	/>
 
-		<div class="bg-primary-300 mx-auto flex rounded-[0.625rem] mt-8 w-full xl:max-w-[48.9rem]">
-			<Avatar rounded src="/gasoline.png" class="h-[5rem] my-auto w-auto ml-[0.94rem]" />
-			<div class="my-auto h-[7.5rem] ml-4 grow flex">
-				<div class="my-auto">
-					<P class="font-inter text-white text-3xl">Gasoline</P>
-					<P class="font-inter text-white text-2xl">The Weeknd</P>
-					<P class="font-inter text-white text-1xl">Dawn FM</P>
-				</div>
+	<div class="bg-primary-300 mx-auto flex rounded-[0.625rem] mt-8 w-full xl:max-w-[48.9rem]">
+		<Avatar rounded src="/gasoline.png" class="h-[5rem] my-auto w-auto ml-[0.94rem]" />
+		<div class="my-auto h-[7.5rem] ml-4 grow flex">
+			<div class="my-auto">
+				<P class="font-inter text-white text-3xl">Gasoline</P>
+				<P class="font-inter text-white text-2xl">The Weeknd</P>
+				<P class="font-inter text-white text-1xl">Dawn FM</P>
 			</div>
-			<Icon src={Pause} class="text-white w-auto my-auto" size="70" />
-			<div class="my-auto mr-[2.48rem]">
-				<Icon src={Forward} class="text-white w-auto" solid size="70" />
-				<P class="font-inter text-white text-center absolute w-[70px]">0/1</P>
-			</div>
+		</div>
+		<Icon src={Pause} class="text-white w-auto my-auto" size="70" />
+		<div class="my-auto mr-[2.48rem]">
+			<Icon src={Forward} class="text-white w-auto" solid size="70" />
+			<P class="font-inter text-white text-center absolute w-[70px]">0/1</P>
 		</div>
 	</div>
-{/await}
+</div>
