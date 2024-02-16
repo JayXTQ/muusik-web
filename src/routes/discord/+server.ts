6ec8cd9bb2e5f812c734dad9dd84207d3dc063c6
Jay <me@jayxtq.xyz>
2024-02-16 20:43:23 +0000

    fix?: not building

--- src/routes/discord/+server.ts
+++ src/routes/discord/+server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-    throw redirect(302, 'https://discord.gg/cmYTNgBnKQ')
+    redirect(302, 'https://discord.gg/cmYTNgBnKQ');
 };
