f2ed708c298751bae6c8925445a9fb64119608c0
Jay <me@jayxtq.xyz>
2024-05-01 11:22:56 +0100

    style: add layout load to layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,9 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { Database } from '../DatabaseDefinitions';
+import type { LayoutLoad } from './$types';
 
-export const load = async ({ fetch, data, depends }) => {
+export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
 	const supabase = createSupabaseLoadClient<Database>({
